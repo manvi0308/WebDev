5cb0f3f1a4f461e24d90379ac5503abed05933bc
MChaddha <147035815+MChaddha@users.noreply.github.com>
2024-02-02 10:13:37 +0530

    added router concepts project

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { InputBox } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
-
+import InputBox from "./components/InputBox";
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("usd");
